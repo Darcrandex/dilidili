de641d29796ce3eeb73bd3ce6bbe7fb5fbd4cbf7
darcrand <darcrandex@gmail.com>
2024-04-01 12:26:11 +0800

    清空项目文件

--- electron/utils.ts
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from 'nanoid'
-
-export function sleep(ms?: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms || 1000))
-}
-
-export function uuid(len = 8) {
-  return customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', len)()
-}
