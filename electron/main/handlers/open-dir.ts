ccb037379c869795f33a17884e68ae67800ad045
darcrand <darcrandex@gmail.com>
2024-04-26 16:20:45 +0800

    clear

--- electron/main/handlers/open-dir.ts
+++
@@ -1,8 +0,0 @@
-import { EChannel } from '@electron/enums'
-import { ipcMain, shell } from 'electron'
-
-export function registerOpenDirHandler() {
-  ipcMain.handle(EChannel.OpenDir, async (_event, dirPath: string) => {
-    shell.openPath(dirPath)
-  })
-}
--- electron/main/utils/common.ts
+++
@@ -1,3 +0,0 @@
-export const uuid = (len = 4) => Math.random().toString(36).slice(-len)
-
-export const sleep = (time = 1000) => new Promise((resolve) => setTimeout(resolve, time))
