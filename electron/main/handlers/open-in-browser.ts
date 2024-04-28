96f786a5bfb456a8f71b996b2806d390edfb7e73
darcrand <darcrandex@gmail.com>
2024-04-28 09:19:06 +0800

    init

--- electron/main/handlers/open-in-browser.ts
+++
@@ -1,9 +0,0 @@
-import { EChannel } from '@electron/enums'
-import { ipcMain, shell } from 'electron'
-
-export function registerOpenInBrowserHandler() {
-  // 使用系统浏览器打开链接
-  ipcMain.handle(EChannel.OpenInBrowser, async (_event, url: string) => {
-    shell.openExternal(url)
-  })
-}
--- src/stores/video-search.ts
+++
@@ -1,8 +0,0 @@
-import { atom, useAtom } from 'jotai'
-
-const stateAtom = atom({ text: '' })
-
-// 用来缓存视频搜索的关键字
-export function useVideoSearch() {
-  return useAtom(stateAtom)
-}
